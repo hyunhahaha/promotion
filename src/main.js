8b5e588df0266cf6604d124ed5aac6a26af109e9
hyunhahaha <pryu3633@naver.com>
2025-02-26 20:58:10 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import "../lib/smooth";
+import "./styles/style.css";
+import { markers } from "../lib/smooth";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { Draggable } from "gsap/Draggable";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+gsap.registerPlugin(ScrollTrigger);
+// ,Draggable,MotionPathPlugin
+
+
+
+
+
+
+
+
+
+
+
+markers();
